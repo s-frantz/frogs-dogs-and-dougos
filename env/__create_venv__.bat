d2b78f4d66e0392d18b550095436b254b762a134
Silas Frantz <silas.frantz@gmail.com>
2024-09-17 18:00:02 -0400

    env

---
+++ env/__create_venv__.bat
@@ -0,0 +1,5 @@
+:: create venv from system standard python
+:: note, go to python.org to download if needed, this project uses...
+:: Python 3.9.5 [MSC v.1928 64 bit (AMD64)]
+python -m venv C:\PythonVenv\FrogsDogsAndDougos
+pause
---
+++ env/__install_requirements__.bat
@@ -0,0 +1,3 @@
+:: install requirements from adjacent .txt
+C:\PythonVenv\FrogsDogsAndDougos\Scripts\python.exe -m pip install -r %~dp0requirements.txt
+pause
